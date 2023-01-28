2ca6c482812a7015c45016ff4e4631c9dc9af031
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-01-28 15:55:50 +0100

    better pass

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $api_key = '47ecd3b21be71c5822571b9f59e52544';
 // Create connection
-$conn = new mysqli('localhost', 'myusername', 'a_password');
+$conn = new mysqli('localhost', 'myusername', '7FHKEPt5D2zAUI2');
 
 // Check connection
 if ($conn->connect_error) {
